2c54db92792662337fda696cc9a102232d4da291
Brian Cantoni <brian@cantoni.org>
2019-10-30 16:10:39 -0700

    Python 3.6+ only

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     name='s3data',
     version=version,
     install_requires=requirements,
-    python_requires='>=3',
+    python_requires='~=3.6',
     author='Brian Cantoni',
     author_email='brian@cantoni.org',
     packages=find_packages(exclude=('tests', 'docs')),
